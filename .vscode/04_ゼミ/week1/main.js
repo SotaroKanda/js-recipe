02f8ce5708786d217c0d333b8a58e1b16cc89488
KandaSoutaro <s-kanda-4z4@eagle.sophia.ac.jp>
2022-04-10 18:49:46 +0900

    ゼミでFizzBuzzを作りました。

---
+++ .vscode/04_ゼミ/week1/main.js
@@ -0,0 +1,17 @@
+const fizzbuzz = function(number) {
+    
+    for (let n = 1; n <= number; n++) {
+      if (n % 3 == 0) {
+        console.log("Fizz")
+      }
+      if(n % 5 == 0){
+        console.log("Buzz")
+      }
+      if(n%3==0 && n%5==0){
+        console.log("FizzBuzz")
+        
+      } else {
+        console.log(n)
+      }
+    }
+  }
